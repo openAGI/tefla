034870c259c8f9e9002da66f83881e398cc24254
Lalit Pant <pant.lalit@gmail.com>
2016-11-15 13:19:34 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tefla',
+    packages=['tefla'],
+    version='0.1',
+    description='Simple end-to-end deep learning with tensorflow. Datasets, data-augmentation, models, training, prediction, and metrics',
+    author='Tefla contributors',
+    author_email='pant.lalit@gmail.com',
+    url='https://github.com/litan/tefla',
+    keywords=['tensorflow', 'deeplearning'],
+    classifiers=[],
+    license='MIT',
+)
