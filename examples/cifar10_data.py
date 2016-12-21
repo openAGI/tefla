c0424acdedf6a017d0ca9ef5dbb516fbb41d1481
Mrinal Haloi <mrinal.haloi11@gmail.com>
2016-12-22 00:28:49 +0530

    minor

--- examples/cifar10_data.py
+++ examples/cifar10_data.py
@@ -12,7 +12,7 @@ from tefla.dataset.base import Dataset
 from tefla.dataset.decoder import Decoder
 from tefla.dataset.dataflow import Dataflow
 
-data_dir='/media/Data/'
+data_dir='/home/artelus_server/data/cifar_10'
 num_readers=8
 
 features_keys = {
@@ -25,6 +25,19 @@ decoder = Decoder(features_keys)
 
 dataset = Dataset('cifar10', decoder, data_dir)
 
-dataflow = Dataflow(dataset, num_readers=num_readers, shuffle=True, capacity=2048)
+dataflow = Dataflow(dataset, num_readers=num_readers, shuffle=True, min_queue_examples=100, capacity=200)
 
-[image, label] = dataflow.get(['image', 'label'])
+[image, label] = dataflow.get(['image', 'label'], [32, 32, 3])
+[image_batch, label_batch] = dataflow.get_batch(32, [0.1 for _ in xrange(0, 10)], [32, 32, 3], init_probs=[0.1 for _ in xrange(0, 10)], threads_per_queue=4)
+
+
+if __name__ == '__main__':
+    sess = tf.Session()
+    coord = tf.train.Coordinator()
+    tf.train.start_queue_runners(sess=sess, coord=coord)
+    image, label = sess.run([image, label])
+    image_batch, label_batch = sess.run([image_batch, label_batch])
+    coord.request_stop()
+    coord.join(stop_grace_period_secs=0.05)
+    print(image_batch.shape)
+    sess.close()
