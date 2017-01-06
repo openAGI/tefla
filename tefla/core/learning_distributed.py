b569b38f79eda534c7750ebf800bd3b29a6b9c16
n3011 <mrinal.haloi11@gmail.com>
2017-01-06 18:06:56 +0530

    minors

--- tefla/core/learning_distributed.py
+++ tefla/core/learning_distributed.py
@@ -10,7 +10,7 @@ import numpy as np
 import tensorflow as tf
 
 from tefla.core.base import Base
-import tefla.core.summary as summary
+# import tefla.core.summary as summary
 import tefla.core.logger as log
 from tefla.utils import util
 
@@ -40,14 +40,85 @@ class DistSupervisedTrainer(Base):
             e.g: total_training_samples/batch_size
         gpu_memory_fraction: amount of gpu memory to use
         is_summary: bool, to write summary or not
+        summarize_gradients: Whether or not add summaries for each gradient.
+        gate_gradients: How to gate the computation of gradients. See tf.Optimizer.
+        aggregation_method: Specifies the method used to combine gradient terms.
+            Valid values are defined in the class `AggregationMethod`.
+        colocate_gradients_with_ops: Whether or not to try colocating the gradients
+            with the ops that generated the
     """
 
-    def __init__(self, model, cnf, clip_by_global_norm=False, **kwargs):
+    def __init__(self, model, cnf, clip_by_global_norm=False, gradient_noise_scale=None, gradient_multipliers=None, aggregation_method=None, colocate_gradients_with_ops=False, **kwargs):
         self.clip_by_global_norm = clip_by_global_norm
+        self.gradient_noise_scale = gradient_noise_scale
+        self.gradient_multipliers = gradient_multipliers
+        self.aggregation_method = aggregation_method
+        self.colocate_gradients_with_ops = colocate_gradients_with_ops
         super(DistSupervisedTrainer, self).__init__(
             self, util.load_module(model), cnf, **kwargs)
 
-    def train(self, train_op, task_id, target, dataset, dataflow, cluster_spec, is_training, start_epoch=1, reuse=None, num_replicas_to_aggregate=-1):
+    def fit(self, task_id, target, dataset, dataflow, cluster_spec, is_training=True, start_epoch=1, reuse=None, num_replicas_to_aggregate=-1, variables_to_train=None):
+        """
+        Train the model on the specified dataset
+
+        Args:
+            task_id: int, id of the task
+            target: name of the TensorFlow target to use. See the tf.Session constructor for
+                how this is interpreted.
+            dataflow: dataflow instance to get data batches
+            dataset: dataset instance to use to access data for training/validation
+            cluster_spec: cluster specifications
+            reuse: whether to resue variables
+            weights_from: str, if not None, initializes model from exisiting weights
+            start_epoch: int,  epoch number to start training from
+                e.g. for retarining set the epoch number you want to resume training from
+            summary_every: int, epoch interval to write summary; higher value means lower frequency
+                of summary writing
+            variables_to_train: an optional list of variables to train. If None, it will
+                  default to all tf.trainable_variables()
+            keep_moving_averages: a bool, keep moving averages of trainable variables
+        """
+        if self.is_summary:
+            self._setup_summaries()
+        self._setup_misc()
+        self._print_info(dataset)
+        self.train(task_id, target, dataset, dataflow, cluster_spec, is_training, start_epoch=1, reuse=None, num_replicas_to_aggregate=-1, variables_to_train=None)
+
+    def _setup_misc(self):
+        self.num_epochs = self.cnf.get('num_epochs', 500)
+        self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        if self.update_ops is not None and len(self.update_ops) == 0:
+            self.update_ops = None
+            # if update_ops is not None:
+            #     self.regularized_training_loss = tf.with_dependencies(update_ops, self.regularized_training_loss)
+
+    def _print_info(self, data_set):
+        log.info('Config:')
+        log.info(pprint.pformat(self.cnf))
+        data_set.print_info()
+        log.info('Max epochs: %d' % self.num_epochs)
+        all_vars = set(tf.all_variables())
+        trainable_vars = set(tf.trainable_variables())
+        non_trainable_vars = all_vars.difference(trainable_vars)
+        log.info("\n---Trainable vars in model:")
+        name_shapes = map(lambda v: (v.name, v.get_shape()), trainable_vars)
+        for n, s in sorted(name_shapes, key=lambda ns: ns[0]):
+            log.info('%s %s' % (n, s))
+
+        log.info("\n---Non Trainable vars in model:")
+        name_shapes = map(lambda v: (v.name, v.get_shape()), non_trainable_vars)
+        for n, s in sorted(name_shapes, key=lambda ns: ns[0]):
+            log.info('%s %s' % (n, s))
+
+        all_ops = tf.get_default_graph().get_operations()
+        log.debug("\n---All ops in graph")
+        names = map(lambda v: v.name, all_ops)
+        for n in sorted(names):
+            log.debug(n)
+
+        self._print_layer_shapes(self.training_end_points, log)
+
+    def train(self, task_id, target, dataset, dataflow, cluster_spec, is_training, start_epoch=1, reuse=None, num_replicas_to_aggregate=-1, variables_to_train=None):
         num_workers = len(cluster_spec.as_dict()['worker'])
         num_parameter_servers = len(cluster_spec.as_dict()['ps'])
         if num_replicas_to_aggregate == -1:
@@ -69,18 +140,13 @@ class DistSupervisedTrainer(Base):
                 global_step = tf.get_variable('global_step', shape=[
                 ], dtype=tf.int64, initializer=tf.zeros_initializer, trainable=False)
                 learning_rate = self.lr_policy.initial_lr
-                try:
-                    n_iters_per_epoch = len(
-                        dataset.training_X) // self.training_iterator.batch_size
-                    n_val_iters_per_epoch = len(
-                        dataset.validation_X) // self.validation_iterator.batch_size
-                except Exception as e:
-                    n_iters_per_epoch = dataset.n_iters_per_epoch
-                    n_val_iters_per_epoch = dataset.n_val_iters_per_epoch
+                n_iters_per_epoch = dataset.n_iters_per_epoch
+                n_val_iters_per_epoch = dataset.n_val_iters_per_epoch
                 self.lr_policy.n_iters_per_epoch = n_iters_per_epoch
 
                 total_loss, opt, val_total_loss = self._setup_model_loss(
                     is_chief, task_id, num_workers, is_training, scope, initial_lr=learning_rate, reuse=None, global_step=None, num_replicas_to_aggregate=-1)
+                train_op = self.create_train_op(total_loss, opt, global_step=global_step, update_ops=None, variables_to_train=None, clip_grad_global_norm=self.clip_grad_global_norm, gradient_noise_scale=self.gradient_noise_scale, gradient_multipliers=self.gradient_multipliers, gate_gradients=tf.Optimizer.GATE_OP, aggregation_method=self.aggregation_method, colocate_gradients_with_ops=self.colocate_gradients_with_ops)
 
                 chief_queue_runners = [opt.get_chief_queue_runner()]
                 init_tokens_op = opt.get_init_tokens_op()
@@ -125,11 +191,13 @@ class DistSupervisedTrainer(Base):
                         batch_train_sizes = []
                         epoch_start_time = time.time()
                         for iteration in range(n_iters_per_epoch):
-                            images, labels = dataflow.batch_inputs(dataset, batch_size=self.cnf.get('batch_size', 32), num_preprocess_threads=self.cnf.get('num_preprocess_threads', 8))
+                            images, labels = dataflow.batch_inputs(dataset, batch_size=self.cnf.get(
+                                'batch_size', 32), num_preprocess_threads=self.cnf.get('num_preprocess_threads', 8))
                             feed_dict_train = {self.inputs: images, self.labels: self._adjust_ground_truth(labels),
                                                self.learning_rate: learning_rate}
                             start_time = time.time()
-                            loss_value, step = sess.run([train_op, global_step], feed_dict=feed_dict_train)
+                            loss_value, step = sess.run(
+                                [train_op, global_step], feed_dict=feed_dict_train)
                             assert not np.isnan(
                                 loss_value), 'Model diverged with loss = NaN'
                             if step > self.cnf.get('max_steps', 10000000):
@@ -145,46 +213,58 @@ class DistSupervisedTrainer(Base):
                             ), step, loss_value, examples_per_sec, duration))
 
                             if is_chief and next_summary_time < time.time():
-                                log.info('Running Summary operation on the chief.')
+                                log.info(
+                                    'Running Summary operation on the chief.')
                                 # summary_str = sess.run(summary_op)
                                 # sv.summary_computed(sess, summary_str)
                                 log.info('Finished running Summary operation.')
 
                             # Determine the next time for running the summary.
                             next_summary_time += self.cnf.get(
-                            'save_summaries_secs', 180)
+                                'save_summaries_secs', 180)
 
                             training_losses.append(loss_value)
                             batch_train_sizes.append(len(images))
-                            learning_rate = self.lr_policy.batch_update(learning_rate, batch_iter_idx)
+                            learning_rate = self.lr_policy.batch_update(
+                                learning_rate, batch_iter_idx)
                             batch_iter_idx += 1
                             log.debug('4. Training batch %d done.' % iteration)
-                        epoch_training_loss = np.average(training_losses, weights=batch_train_sizes)
-                        epoch_duration = time.time() - epoch_start_time
+                        epoch_training_loss = np.average(
+                            training_losses, weights=batch_train_sizes)
+                        # epoch_duration = time.time() - epoch_start_time
                         # Validation prediction and metrics
                         validation_losses = []
-                        batch_validation_metrics = [[] for _, _ in self.validation_metrics_def]
+                        batch_validation_metrics = [
+                            [] for _, _ in self.validation_metrics_def]
                         epoch_validation_metrics = []
                         batch_validation_sizes = []
                         for iteration in range(n_val_iters_per_epoch):
-                            val_images, val_labels = dataflow.batch_inputs(dataset, batch_size=self.cnf.get('batch_size', 32), num_preprocess_threads=self.cnf.get('num_preprocess_threads', 8))
+                            val_images, val_labels = dataflow.batch_inputs(dataset, batch_size=self.cnf.get(
+                                'batch_size', 32), num_preprocess_threads=self.cnf.get('num_preprocess_threads', 8))
                             feed_dict_val = {self.validation_inputs: val_images, self.validation_labels: self._adjust_ground_truth(val_labels),
-                                               self.learning_rate: learning_rate}
-                            log.debug('6. Loading batch %d validation data done.' % iteration)
-                            log.debug('7. Running validation steps without summary...')
+                                             self.learning_rate: learning_rate}
+                            log.debug(
+                                '6. Loading batch %d validation data done.' % iteration)
+                            log.debug(
+                                '7. Running validation steps without summary...')
                             validation_predictions_e, validation_loss_e = sess.run(
                                 [self.validation_predictions, val_total_loss],
                                 feed_dict=feed_dict_val)
-                            log.debug('7. Running validation steps without summary done.')
+                            log.debug(
+                                '7. Running validation steps without summary done.')
                             validation_losses.append(validation_loss_e)
                             batch_validation_sizes.append(len(val_images))
 
                             for i, (_, metric_function) in enumerate(self.validation_metrics_def):
-                                metric_score = metric_function(val_labels, validation_predictions_e)
-                                batch_validation_metrics[i].append(metric_score)
-                            log.debug('8. Validation batch %d done' % batch_num)
+                                metric_score = metric_function(
+                                    val_labels, validation_predictions_e)
+                                batch_validation_metrics[
+                                    i].append(metric_score)
+                            log.debug('8. Validation batch %d done' %
+                                      iteration)
 
-                        epoch_validation_loss = np.average(validation_losses, weights=batch_validation_sizes)
+                        epoch_validation_loss = np.average(
+                            validation_losses, weights=batch_validation_sizes)
                         for i, (_, _) in enumerate(self.validation_metrics_def):
                             epoch_validation_metrics.append(
                                 np.average(batch_validation_metrics[i], weights=batch_validation_sizes))
@@ -203,7 +283,7 @@ class DistSupervisedTrainer(Base):
 
                         if is_chief:
                             saver.save(sess, os.path.join(self.weights_dir,
-                                                  'model.ckpt'), global_step=step)
+                                                          'model.ckpt'), global_step=step)
 
                         epoch_info = dict(
                             epoch=epoch,
@@ -279,6 +359,7 @@ class DistSupervisedTrainer(Base):
                 loss_name = re.sub('%s_[0-9]*/' %
                                    self.cnf['TOWER_NAME'], '', l.op.name)
                 tf.scalar_summary(loss_name, l)
+            return losses, total_loss
         else:
             self.validation_end_points = model(
                 images, is_training=is_training, reuse=resue)
@@ -288,8 +369,10 @@ class DistSupervisedTrainer(Base):
             else:
                 total_loss = self._loss_regression(self.validation_end_points[
                                                    'logits'], labels, is_training)
+            self.validation_predictions = self.validation_end_points[
+                'predictions']
 
-        return losses, total_loss
+            return total_loss
 
     def _setup_model_loss(self, is_chief, task_id, num_workers, is_training, scope, initial_lr=0.1, reuse=None, global_step=None, num_replicas_to_aggregate=-1):
         self.inputs = tf.placeholder(tf.float32, shape=(None, self.model.crop_size[
@@ -301,7 +384,7 @@ class DistSupervisedTrainer(Base):
 
         losses, total_loss = self._tower_loss(
             scope, self.model, self.inputs, self.labels, is_training, reuse, is_classification=True)
-        val_losses, val_total_loss = self._tower_loss(
+        val_total_loss = self._tower_loss(
             scope, self.model, self.validation_inputs, self.validation_labels, is_training=False, reuse=True, is_classification=True)
 
         if is_chief:
@@ -340,7 +423,7 @@ class DistSupervisedTrainer(Base):
             variables_to_train: an optional list of variables to train. If None, it will
                 default to all tf.trainable_variables().
             clip_grad_global_norm: A bool, performs gradient clipping using global norm if True
-                else performs gradient clipping using local norm. 
+                else performs gradient clipping using local norm.
             gradient_noise_scale: if not None, add noises to the gradients
             gradient_multipliers: if not None, a dict, multiples gradient with given args
             gate_gradients: How to gate the computation of gradients. See tf.Optimizer.
