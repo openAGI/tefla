8c9cc06465ec7c2c72db3fe426e82c6f961dd5c9
Mrinal Haloi <mrinal.haloi11@gmail.com>
2017-03-17 21:15:48 +0530

    minor

--- tefla/tests/test_standardizer.py
+++ tefla/tests/test_standardizer.py
@@ -12,7 +12,8 @@ def _reset_graph():
 
 def test_np_tf_aggregate():
     standardizer = AggregateStandardizer(
-        mean=np.array([108.64628601, 75.86886597, 54.34005737], dtype=np.float32),
+        mean=np.array([108.64628601, 75.86886597, 54.34005737],
+                      dtype=np.float32),
         std=np.array([70.53946096, 51.71475228, 43.03428563], dtype=np.float32),
         u=np.array([[-0.56543481, 0.71983482, 0.40240142],
                     [-0.5989477, -0.02304967, -0.80036049],
@@ -21,7 +22,8 @@ def test_np_tf_aggregate():
         sigma=0.5
     )
     standardizertf = AggregateStandardizerTF(
-        mean=np.array([108.64628601, 75.86886597, 54.34005737], dtype=np.float32),
+        mean=np.array([108.64628601, 75.86886597, 54.34005737],
+                      dtype=np.float32),
         std=np.array([70.53946096, 51.71475228, 43.03428563], dtype=np.float32),
         u=np.array([[-0.56543481, 0.71983482, 0.40240142],
                     [-0.5989477, -0.02304967, -0.80036049],
@@ -32,9 +34,14 @@ def test_np_tf_aggregate():
 
     sess = tf.Session()
 
-    im_tf = tf.read_file('/home/ishant/Downloads/segmentation2.jpg')
-    im_tf = tf.image.decode_jpeg(im_tf)
-    im_np = np.asarray(im_tf.eval(session=sess), dtype=np.float32).transpose(2, 1, 0)
+    # im_tf = tf.read_file('/home/ishant/Downloads/segmentation2.jpg')
+    # im_tf = tf.image.decode_jpeg(im_tf)
+    # im_np = np.asarray(im_tf.eval(session=sess), dtype=np.float32).transpose(2, 1, 0)
+    im_np = np.random.normal(50.0, 2.5,
+                             size=(200, 200, 3))
+    im_np = np.clip(im_np, 0.0, 255.0)
+    im_tf = im_np
+    im_np = np.asarray(im_np.transpose(2, 1, 0), dtype=np.float32)
     im_st = standardizer(im_np, False)
     im_st = im_st.transpose(1, 2, 0)
     im_tf = tf.transpose(im_tf, perm=[1, 0, 2])
