8c96981c2bc88b71c23060698c37116a19643018
Mrinal Haloi <mrinal.haloi11@gmail.com>
2017-03-23 01:21:38 +0530

    minor

--- tefla/da/standardizer.py
+++ tefla/da/standardizer.py
@@ -167,10 +167,8 @@ class AggregateStandardizerTF(object):
 
         """
         if color_vec is None:
-            if not sigma > 0.0:
-                color_vec = tf.zeros(shape=(3,), dtype=tf.float32)
-            else:
-                color_vec = tf.random_normal(shape=(3,), mean=0.0, std=sigma)
+            # tf.zeros(shape=(3,), dtype=tf.float32))
+            color_vec = tf.random_normal(shape=(3,), mean=0.0, stddev=sigma)
 
         alpha = tf.multiply(tf.to_float(color_vec), self.ev)
         # noise = tf.reshape(tf.tensordot(self.u, alpha, 1), shape=(1, 1, 3))
