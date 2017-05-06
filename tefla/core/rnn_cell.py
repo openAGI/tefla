af4f973111b5b3e6ddefc03ac5606a86be1509ee
n3011 <mrinal.haloi11@gmail.com>
2017-05-06 20:39:15 +0530

    update lstm cells

--- tefla/core/rnn_cell.py
+++ tefla/core/rnn_cell.py
@@ -25,10 +25,9 @@ class BasicRNNCell(core_rnn_cell.RNNCell):
         layer_norm_args: optional dict, layer_norm arguments
         trainable: If `True` also add variables to the graph collection
             `GraphKeys.TRAINABLE_VARIABLES` (see tf.Variable).
-        outputs_collections: The collections to which the outputs are added.
     """
 
-    def __init__(self, num_units, reuse, trainable=True, input_size=None, activation=tf.tanh, layer_norm=None, layer_norm_args=None, outputs_collections=None):
+    def __init__(self, num_units, reuse, trainable=True, input_size=None, activation=tf.tanh, layer_norm=None, layer_norm_args=None):
         if input_size is not None:
             log.warn("%s: The input_size parameter is deprecated.", self)
         self._num_units = num_units
@@ -37,7 +36,6 @@ class BasicRNNCell(core_rnn_cell.RNNCell):
         self.trainable = trainable
         self.layer_norm = layer_norm
         self.layer_norm_args = layer_norm_args or {}
-        self.outputs_collections = outputs_collections
 
     @property
     def state_size(self):
@@ -55,8 +53,6 @@ class BasicRNNCell(core_rnn_cell.RNNCell):
             if self.layer_norm is not None:
                 output = self.layer_norm(
                     output, self.reuse, trainable=self.trainable, **self.layer_norm_args)
-            output = _collect_named_outputs(
-                self.outputs_collections, scope + '_output', output)
         return output, output
 
 
@@ -91,10 +87,9 @@ class LSTMCell(core_rnn_cell.RNNCell):
         dropout_seed: (optional) integer, the randomness seed.
         trainable: If `True` also add variables to the graph collection
             `GraphKeys.TRAINABLE_VARIABLES` (see tf.Variable).
-        outputs_collections: The collections to which the outputs are added.
     """
 
-    def __init__(self, num_units, reuse, trainable=True, forget_bias=1.0, input_size=None, activation=tf.tanh, inner_activation=tf.sigmoid, keep_prob=1.0, dropout_seed=None, cell_clip=None, layer_norm=None, layer_norm_args=None, outputs_collections=None):
+    def __init__(self, num_units, reuse, trainable=True, forget_bias=1.0, input_size=None, activation=tf.tanh, inner_activation=tf.sigmoid, keep_prob=1.0, dropout_seed=None, cell_clip=None, layer_norm=None, layer_norm_args=None):
         if input_size is not None:
             ValueError("%s: the input_size parameter is deprecated." % self)
         self._num_units = num_units
@@ -108,7 +103,6 @@ class LSTMCell(core_rnn_cell.RNNCell):
         self._dropout_seed = dropout_seed
         self.trainable = trainable
         self.reuse = reuse
-        self.outputs_collections = outputs_collections
 
     @property
     def state_size(self):
@@ -164,10 +158,6 @@ class LSTMCell(core_rnn_cell.RNNCell):
 
             new_state = core_rnn_cell.LSTMStateTuple(new_c, new_h)
 
-            new_h = _collect_named_outputs(
-                self.outputs_collections, scope + '_h', new_h)
-            new_state = _collect_named_outputs(
-                self.outputs_collections, scope + '_state', new_state)
             return new_h, new_state
 
 
@@ -194,14 +184,13 @@ class AttentionCell(core_rnn_cell.RNNCell):
             built from inputs and attention. Derived from the input tensor by default.
         trainable: If `True` also add variables to the graph collection
             `GraphKeys.TRAINABLE_VARIABLES` (see tf.Variable).
-        outputs_collections: The collections to which the outputs are added.
 
     Raises:
         TypeError: if cell is not an RNNCell.
         ValueError: Cell state should be tuple of states
     """
 
-    def __init__(self, cell, attn_length, reuse, trainable=True, attn_size=None, attn_vec_size=None, input_size=None, layer_norm=None, layer_norm_args=None, outputs_collections=None):
+    def __init__(self, cell, attn_length, reuse, trainable=True, attn_size=None, attn_vec_size=None, input_size=None, layer_norm=None, layer_norm_args=None):
         if not isinstance(cell, core_rnn_cell.RNNCell):
             raise TypeError("The parameter cell is not RNNCell.")
         if not helper.is_sequence(cell.state_size):
@@ -222,7 +211,6 @@ class AttentionCell(core_rnn_cell.RNNCell):
         self.layer_norm_args = layer_norm_args or {}
         self.reuse = reuse
         self.trainable = trainable
-        self.outputs_collections = outputs_collections
 
     @property
     def state_size(self):
@@ -261,10 +249,6 @@ class AttentionCell(core_rnn_cell.RNNCell):
                 new_state = self.layer_norm(
                     new_state, self.reuse, trainable=self.trainable, **self.layer_norm_args)
 
-            output = _collect_named_outputs(
-                self.outputs_collections, scope + '_output', output)
-            new_state = _collect_named_outputs(
-                self.outputs_collections, scope + '_state', new_state)
             return output, new_state
 
 
@@ -282,10 +266,9 @@ class GRUCell(core_rnn_cell.RNNCell):
         layer_norm_args: optional dict, layer_norm arguments
         trainable: If `True` also add variables to the graph collection
             `GraphKeys.TRAINABLE_VARIABLES` (see tf.Variable).
-        outputs_collections: The collections to which the outputs are added.
     """
 
-    def __init__(self, num_units, reuse, trainable=True, input_size=None, activation=tf.tanh, inner_activation=tf.sigmoid, b_init=1.0, layer_norm=None, layer_norm_args=None, outputs_collections=None):
+    def __init__(self, num_units, reuse, trainable=True, input_size=None, activation=tf.tanh, inner_activation=tf.sigmoid, b_init=1.0, layer_norm=None, layer_norm_args=None):
         if input_size is not None:
             log.warn("%s: The input_size parameter is deprecated.", self)
         self._num_units = num_units
@@ -334,8 +317,6 @@ class GRUCell(core_rnn_cell.RNNCell):
                 new_h = self.layer_norm(
                     new_h, self.reuse, trainable=self.trainable)
 
-            new_h = _collect_named_outputs(
-                self.outputs_collections, scope + '_h', new_h)
         return new_h, new_h
 
 
@@ -352,35 +333,196 @@ class MultiRNNCell(core_rnn_cell.RNNCell):
 
     def __init__(self, cells, state_is_tuple=True):
         if not cells:
-            raise ValueError("Must specify at least one cell for MultiRNNCell.")
+            raise ValueError(
+                "Must specify at least one cell for MultiRNNCell.")
         self._cells = cells
         if any(helper.is_sequence(c.state_size) for c in self._cells):
             raise ValueError("Cell state should be tuple of states")
 
-        @property
-        def state_size(self):
-            return sum([cell.state_size for cell in self._cells])
+    @property
+    def state_size(self):
+        return sum([cell.state_size for cell in self._cells])
 
-        @property
-        def output_size(self):
-            return self._cells[-1].output_size
+    @property
+    def output_size(self):
+        return self._cells[-1].output_size
 
-        def __call__(self, inputs, state, scope='multi_rnn_cell'):
-            """Run this multi-layer cell on inputs, starting from state."""
-            with tf.variable_scope(scope):
-                cur_inp = inputs
-                new_states = []
-                for i, cell in enumerate(self._cells):
-                    with tf.variable_scope("cell_%d" % i):
-                        if not helper.is_sequence(state):
-                            raise ValueError("Expected state to be a tuple of length %d, but received: %s" % (
-                                len(self.state_size), state))
-                        cur_state = state[i]
-                        cur_inp, new_state = cell(cur_inp, cur_state)
-                        new_states.append(new_state)
-            new_states = (
-                tuple(new_states) if self._state_is_tuple else tf.concat(new_states, 1))
-            return cur_inp, new_states
+    def __call__(self, inputs, state, scope='multi_rnn_cell'):
+        """Run this multi-layer cell on inputs, starting from state."""
+        with tf.variable_scope(scope):
+            cur_inp = inputs
+            new_states = []
+            for i, cell in enumerate(self._cells):
+                with tf.variable_scope("cell_%d" % i):
+                    if not helper.is_sequence(state):
+                        raise ValueError("Expected state to be a tuple of length %d, but received: %s" % (
+                            len(self.state_size), state))
+                    cur_state = state[i]
+                    cur_inp, new_state = cell(cur_inp, cur_state)
+                    new_states.append(new_state)
+        new_states = (
+            tuple(new_states) if self._state_is_tuple else tf.concat(new_states, 1))
+        return cur_inp, new_states
+
+
+class GLSTMCell(core_rnn_cell.RNNCell):
+    """Group LSTM cell (G-LSTM).
+    The implementation is based on:
+    https://arxiv.org/abs/1703.10722
+    O. Kuchaiev and B. Ginsburg
+    "Factorization Tricks for LSTM Networks", ICLR 2017 workshop.
+    """
+
+    def __init__(self, num_units, reuse, initializer=None, num_proj=None, number_of_groups=1, forget_bias=1.0, activation=tf.tanh):
+        """Initialize the parameters of G-LSTM cell.
+
+        Args:
+            num_units: int, The number of units in the G-LSTM cell
+            initializer: (optional) The initializer to use for the weight and
+                projection matrices.
+            num_proj: (optional) int, The output dimensionality for the projection
+                matrices.  If None, no projection is performed.
+            number_of_groups: (optional) int, number of groups to use.
+                If `number_of_groups` is 1, then it should be equivalent to LSTM cell
+            forget_bias: Biases of the forget gate are initialized by default to 1
+                in order to reduce the scale of forgetting at the beginning of
+                the training.
+            activation: Activation function of the inner states.
+            reuse: (optional) Python boolean describing whether to reuse variables
+                in an existing scope.  If not `True`, and the existing scope already
+                has the given variables, an error is raised.
+
+        Raises:
+            ValueError: If `num_units` or `num_proj` is not divisible by
+                `number_of_groups`.
+        """
+        self._num_units = num_units
+        self._initializer = initializer
+        self._num_proj = num_proj
+        self._forget_bias = forget_bias
+        self._activation = activation
+        self._number_of_groups = number_of_groups
+        self.reuse = reuse
+
+        if self._num_units % self._number_of_groups != 0:
+            raise ValueError("num_units must be divisible by number_of_groups")
+        if self._num_proj:
+            if self._num_proj % self._number_of_groups != 0:
+                raise ValueError(
+                    "num_proj must be divisible by number_of_groups")
+            self._group_shape = [int(self._num_proj / self._number_of_groups),
+                                 int(self._num_units / self._number_of_groups)]
+        else:
+            self._group_shape = [int(self._num_units / self._number_of_groups),
+                                 int(self._num_units / self._number_of_groups)]
+
+        if num_proj:
+            self._state_size = core_rnn_cell.LSTMStateTuple(
+                num_units, num_proj)
+            self._output_size = num_proj
+        else:
+            self._state_size = core_rnn_cell.LSTMStateTuple(
+                num_units, num_units)
+            self._output_size = num_units
+
+    @property
+    def state_size(self):
+        return self._state_size
+
+    @property
+    def output_size(self):
+        return self._output_size
+
+    def _get_input_for_group(self, inputs, group_id, group_size):
+        """Slices inputs into groups to prepare for processing by cell's groups
+
+        Args:
+            inputs: cell input or it's previous state,
+               a Tensor, 2D, [batch x num_units]
+            group_id: group id, a Scalar, for which to prepare input
+            group_size: size of the group
+
+        Returns:
+            subset of inputs corresponding to group "group_id",
+            a Tensor, 2D, [batch x num_units/number_of_groups]
+        """
+        return tf.slice(input_=inputs, begin=[0, group_id * group_size], size=[self._batch_size, group_size], name=("GLSTM_group%d_input_generation" % group_id))
+
+    def __call__(self, inputs, state, scope='glstm_cell'):
+        """Run one step of G-LSTM.
+
+        Args:
+            inputs: input Tensor, 2D, [batch x num_units].
+            state: this must be a tuple of state Tensors, both `2-D`,
+                with column sizes `c_state` and `m_state`.
+
+        Returns:
+            A tuple containing:
+                - A `2-D, [batch x output_dim]`, Tensor representing the output of the
+                G-LSTM after reading `inputs` when previous state was `state`.
+                Here output_dim is: num_proj if num_proj was set, num_units otherwise.
+                - LSTMStateTuple representing the new state of G-LSTM  cell
+                after reading `inputs` when the previous state was `state`.
+
+        Raises:
+            ValueError: If input size cannot be inferred from inputs via static shape inference.
+        """
+        (c_prev, m_prev) = state
+
+        self._batch_size = inputs.shape[0].value or tf.shape(inputs)[0]
+        dtype = inputs.dtype
+        with tf.variable_scope(scope, initializer=self._initializer):
+            i_parts = []
+            j_parts = []
+            f_parts = []
+            o_parts = []
+
+            for group_id in range(self._number_of_groups):
+                with tf.variable_scope("group%d" % group_id):
+                    x_g_id = tf.concat([self._get_input_for_group(inputs, group_id, self._group_shape[
+                                       0]), self._get_input_for_group(m_prev, group_id, self._group_shape[0])], axis=1)
+                    R_k = _linear(
+                        x_g_id, 4 * self._group_shape[1], self.reuse, use_bias=False, name=scope + "group%d" % group_id)
+                    i_k, j_k, f_k, o_k = tf.split(R_k, 4, 1)
+
+                i_parts.append(i_k)
+                j_parts.append(j_k)
+                f_parts.append(f_k)
+                o_parts.append(o_k)
+
+            bi = tf.get_variable(name="bias_i",
+                                 shape=[self._num_units],
+                                 dtype=dtype,
+                                 initializer=tf.constant_initializer(0.0, dtype=dtype))
+            bj = tf.get_variable(name="bias_j",
+                                 shape=[self._num_units],
+                                 dtype=dtype,
+                                 initializer=tf.constant_initializer(0.0, dtype=dtype))
+            bf = tf.get_variable(name="bias_f",
+                                 shape=[self._num_units],
+                                 dtype=dtype,
+                                 initializer=tf.constant_initializer(0.0, dtype=dtype))
+            bo = tf.get_variable(name="bias_o",
+                                 shape=[self._num_units],
+                                 dtype=dtype,
+                                 initializer=tf.constant_initializer(0.0, dtype=dtype))
+
+            i = tf.nn.bias_add(tf.concat(i_parts, axis=1), bi)
+            j = tf.nn.bias_add(tf.concat(j_parts, axis=1), bj)
+            f = tf.nn.bias_add(tf.concat(f_parts, axis=1), bf)
+            o = tf.nn.bias_add(tf.concat(o_parts, axis=1), bo)
+
+        c = (tf.sigmoid(f + self._forget_bias) *
+             c_prev + tf.sigmoid(i) * tf.tanh(j))
+        m = tf.sigmoid(o) * self._activation(c)
+
+        if self._num_proj is not None:
+            with tf.variable_scope("projection"):
+                m = _linear(m, self._num_proj, self.reuse,
+                            use_bias=False, name=scope + 'projection')
+
+        new_state = core_rnn_cell.LSTMStateTuple(c, m)
+        return m, new_state
 
 
 class DropoutWrapper(core_rnn_cell.RNNCell):
