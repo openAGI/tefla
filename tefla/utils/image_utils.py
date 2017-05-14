11774a1e820e93be6622950a361ded0f5ab7e870
n3011 <mrinal.haloi11@gmail.com>
2017-05-15 02:04:49 +0530

    calibraon, IPM

--- tefla/utils/image_utils.py
+++ tefla/utils/image_utils.py
@@ -5,6 +5,8 @@ import h5py
 import os
 import math
 import random
+import pickle
+from tqdm import tqdm
 import pprint
 import scipy.misc
 import tensorflow as tf
@@ -131,3 +133,150 @@ def transform(image, npx=64, is_crop=True):
 
 def inverse_transform(images):
     return (images + 1.) / 2.
+
+
+class PerspectiveTransformer:
+
+    def __init__(self, src, dst):
+        """Perspective and Inverse perspective transformer
+
+        Args:
+            src: Source coordinates for perspective transformation
+            dst: Destination coordinates for perspective transformation
+        """
+        self.src = src
+        self.dst = dst
+        self.M = cv2.getPerspectiveTransform(src, dst)
+        self.M_inv = cv2.getPerspectiveTransform(dst, src)
+
+    def warp(self, img, matrix):
+        return cv2.warpPerspective(img, matrix, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
+
+    def transform(self, img, offset=0):
+        if offset == 0:
+            return self.warp(img, self.M)
+        else:
+            src = self.src.copy()
+            src[:, 0] = src[:, 0] + offset
+
+            dst = self.dst.copy()
+            dst[:, 0] = dst[:, 0] + offset
+
+            M_inv = cv2.getPerspectiveTransform(src, dst)
+            return self.warp(img, M_inv)
+
+    def inverse_transform(self, img, offset=0):
+        if offset == 0:
+            return self.warp(img, self.M_inv)
+        else:
+            src = self.src.copy()
+            src[:, 0] = src[:, 0] + offset
+
+            dst = self.dst.copy()
+            dst[:, 0] = dst[:, 0] + offset
+
+            M_inv = cv2.getPerspectiveTransform(dst, src)
+            return self.warp(img, M_inv)
+
+
+def calculate_camera_calibration(calib_path, rows, cols, cal_image_size):
+    """Calculates the camera calibration based on chessboard images.
+
+    Args:
+        calib_path: calibration data (imgs) dir path
+        rows: number of rows on chessboard
+        cols: number of columns on chessboard
+
+    Returns:
+        a `dict` with calibration points
+    """
+    objp = np.zeros((rows * cols, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:cols, 0:rows].T.reshape(-1, 2)
+
+    objpoints = []
+    imgpoints = []
+
+    images = glob(calib_path)
+    cal_images = np.zeros((len(images), *cal_image_size), dtype=np.uint8)
+
+    successfull_cnt = 0
+    for idx, fname in enumerate(tqdm(images, desc='Processing image')):
+        img = scipy.misc.imread(fname)
+        if img.shape[0] != cal_image_size[0] or img.shape[1] != cal_image_size[1]:
+            img = scipy.misc.imresize(img, cal_image_size)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (cols, rows), None)
+
+        if ret:
+            successfull_cnt += 1
+
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            img = cv2.drawChessboardCorners(img, (cols, rows), corners, ret)
+            cal_images[idx] = img
+
+    print("%s/%s camera calibration images processed." %
+          (successfull_cnt, len(images)))
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objpoints, imgpoints, cal_image_size[:-1], None, None)
+
+    calibration = {'objpoints': objpoints,
+                   'imgpoints': imgpoints,
+                   'cal_images': cal_images,
+                   'mtx': mtx,
+                   'dist': dist,
+                   'rvecs': rvecs,
+                   'tvecs': tvecs}
+
+    return calibration
+
+
+def get_camera_calibration(cal_calib_points, calib_data_path=None, rows=6, cols=9, cal_image_shape=None, calib_out_path=None):
+    """The camera calibration will be
+    calculated and stored on disk or loaded.
+    """
+    if cal_calib_points:
+        calibration = calculate_camera_calibration(
+            calib_data_path, rows, cols, cal_image_shape)
+        with open(calib_out_path, 'wb') as f:
+            pickle.dump(calibration, file=f)
+    else:
+        with open(calib_out_path, 'rb') as f:
+            calibration = pickle.load(f)
+    return calibration
+
+
+class CameraCalibrator:
+
+    def __init__(self, calibration=None, calib_data_path=None, rows=6, cols=9, cal_image_shape=None):
+        """Helper class to remove lens distortion from images (camera calibration)
+
+        Args:
+            calibration: precalculated calibration matrices
+            calib_data_path: path to data for camera calibration
+            rows: number of rows on chessboard
+            cols: number of columns on chessboard
+            cal_image_shape: calibration image shape
+        """
+        if calibration is not None:
+            self.ret, self.mtx, self.dist, self.rvecs, self.tvecs = cv2.calibrateCamera(
+                calibration['objpoints'], calibration['imgpoints'], cal_image_shape, None, None)
+        else:
+            calibration = calculate_camera_calibration(
+                calib_data_path, rows, cols, cal_image_shape)
+            self.mtx = calibration['mtx']
+            self.dist = calibration['dist']
+
+    def undistort(self, img):
+        """ Restore image from camera distrotation using calibration matrics
+
+        Args:
+            img: input image
+
+        Returns:
+            restored image
+        """
+        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
