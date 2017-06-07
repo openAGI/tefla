75f4c9acbf25bcbb7c75db7782dc956db6112ce1
n3011 <mrinal.haloi11@gmail.com>
2017-06-07 22:14:54 +0530

    minor

--- examples/datasets/cifar10_data_download_process_tfrecords.py
+++ examples/datasets/cifar10_data_download_process_tfrecords.py
@@ -71,23 +71,29 @@ def _add_to_tfrecord(filename, tfrecord_writer, split_name=None, offset=0):
         with tf.Session('') as sess:
 
             for j in range(num_images):
-                sys.stdout.write('\r>> Reading file [%s] image %d/%d' % (filename, offset + j + 1, offset + num_images))
+                sys.stdout.write(
+                    '\r>> Reading file [%s] image %d/%d' % (filename, offset + j + 1, offset + num_images))
                 sys.stdout.flush()
 
                 image = np.squeeze(images[j]).transpose((1, 2, 0))
                 label = labels[j]
                 filename_image = filenames[j]
 
-                jpg_string = sess.run(encoded_image, feed_dict={image_placeholder: image})
+                jpg_string = sess.run(encoded_image, feed_dict={
+                                      image_placeholder: image})
 
-                example = tfrecords.convert_to_example(filename_image, jpg_string, label, 'cifar', _IMAGE_SIZE, _IMAGE_SIZE)
+                example = tfrecords.convert_to_example(
+                    filename_image, jpg_string, label, 'cifar', _IMAGE_SIZE, _IMAGE_SIZE)
                 tfrecord_writer.write(example.SerializeToString())
 
         return offset + num_images
 
 
-def _get_output_filename(dataset_dir, split_name):
-    return '%s/cifar10_%s.tfrecord' % (dataset_dir, split_name)
+def _get_output_filename(dataset_dir, train, split_name):
+    if train:
+        return '%s/%s/cifar10_%s.tfrecord' % (dataset_dir, 'train', split_name)
+    else:
+        return '%s/%s/cifar10_%s.tfrecord' % (dataset_dir, 'val', split_name)
 
 
 def _clean_up_temporary_files(dataset_dir):
@@ -104,7 +110,8 @@ def download_and_uncompress_tarball(tarball_url, dataset_dir):
     filepath = os.path.join(dataset_dir, filename)
 
     def _progress(count, block_size, total_size):
-        sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename, float(count * block_size) / float(total_size) * 100.0))
+        sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+            filename, float(count * block_size) / float(total_size) * 100.0))
         sys.stdout.flush()
     filepath, _ = urllib.request.urlretrieve(tarball_url, filepath, _progress)
     print()
@@ -124,14 +131,15 @@ def write_label_file(labels_to_class_names, dataset_dir, filename='labels.txt'):
 def main(dataset_dir):
     if not tf.gfile.Exists(dataset_dir):
         tf.gfile.MakeDirs(dataset_dir)
-
+    tf.gfile.MkDir(os.path.join(dataset_dir, 'train'))
+    tf.gfile.MkDir(os.path.join(dataset_dir, 'val'))
     if tf.gfile.Exists(dataset_dir + '/cifar-10-batches-py'):
         print('Dataset files already downloaded.')
     else:
         download_and_uncompress_tarball(_DATA_URL, dataset_dir)
 
-    training_filename = _get_output_filename(dataset_dir, 'train')
-    testing_filename = _get_output_filename(dataset_dir, 'test')
+    training_filename = _get_output_filename(dataset_dir, True,  'train')
+    testing_filename = _get_output_filename(dataset_dir, False, 'val')
 
     if tf.gfile.Exists(training_filename) and tf.gfile.Exists(testing_filename):
         print('Dataset files already exist. Exiting without re-creating them.')
@@ -141,12 +149,16 @@ def main(dataset_dir):
     with tf.python_io.TFRecordWriter(training_filename) as tfrecord_writer:
         offset = 0
         for i in range(_NUM_TRAIN_FILES):
-            filename = os.path.join(dataset_dir, 'cifar-10-batches-py', 'data_batch_%d' % (i + 1))  # 1-indexed.
-            offset = _add_to_tfrecord(filename, tfrecord_writer, offset=offset, split_name='train')
+            # 1-indexed.
+            filename = os.path.join(
+                dataset_dir, 'cifar-10-batches-py', 'data_batch_%d' % (i + 1))
+            offset = _add_to_tfrecord(
+                filename, tfrecord_writer, offset=offset, split_name='train')
 
     # Next, process the testing data:
     with tf.python_io.TFRecordWriter(testing_filename) as tfrecord_writer:
-        filename = os.path.join(dataset_dir, 'cifar-10-batches-py', 'test_batch')
+        filename = os.path.join(
+            dataset_dir, 'cifar-10-batches-py', 'test_batch')
         _add_to_tfrecord(filename, tfrecord_writer, split_name='test')
 
     # Finally, write the labels file:
