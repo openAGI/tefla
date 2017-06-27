59c38b249e5a4be5202a5ba6323a86a4ca23d2cd
n3011 <mrinal.haloi11@gmail.com>
2017-06-27 15:47:34 +0530

    log file name set

--- tefla/train_seg.py
+++ tefla/train_seg.py
@@ -37,20 +37,20 @@ import logging
               help='Path to initial weights file.')
 @click.option('--loss_type', default='cross_entropy', show_default=True,
               help='Loss fuction type.')
+@click.option('--log_file_name', default='train_seg.log', show_default=True,
+              help='Log file name.')
 @click.option('--is_summary', default=False, show_default=True,
               help='Path to initial weights file.')
-def main(model, training_cnf, data_dir, parallel, start_epoch, weights_from, weights_dir, resume_lr, gpu_memory_fraction, is_summary, loss_type):
+def main(model, training_cnf, data_dir, parallel, start_epoch, weights_from, weights_dir, resume_lr, gpu_memory_fraction, is_summary, loss_type, log_file_name):
     with tf.Graph().as_default():
         model_def = util.load_module(model)
         model = model_def.model
         cnf = util.load_module(training_cnf).cnf
 
-        util.init_logging('train.log', file_log_level=logging.INFO,
-                          console_log_level=logging.INFO)
         if weights_from:
             weights_from = str(weights_from)
 
-        trainer = SupervisedLearner(model, cnf, log_file_name='train_seg.log', resume_lr=resume_lr, classification=cnf[
+        trainer = SupervisedLearner(model, cnf, log_file_name=log_file_name, resume_lr=resume_lr, classification=cnf[
                                     'classification'], gpu_memory_fraction=gpu_memory_fraction, num_classes=15, is_summary=is_summary, loss_type=loss_type, verbosity=1)
         trainer.fit(data_dir, weights_from=weights_from, weights_dir=weights_dir, start_epoch=start_epoch,
                     summary_every=399, keep_moving_averages=True)
