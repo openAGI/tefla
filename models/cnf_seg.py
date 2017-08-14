dc066bdf2f80bbbd01b614dee27bace3e53d5459
n3011 <mrinal.haloi11@gmail.com>
2017-08-14 23:03:54 +0530

    minor

--- models/cnf_seg.py
+++ models/cnf_seg.py
@@ -4,6 +4,7 @@ import numpy as np
 import tensorflow as tf
 from tefla.core.lr_policy import PolyDecayPolicy, StepDecayPolicy
 from tefla.da.standardizer import SamplewiseStandardizerTF
+from tefla.da.standardizer import SamplewiseStandardizer
 from tefla.utils import util
 
 num_classes = 10
@@ -12,11 +13,14 @@ cnf = {
     'batch_size_train': 1,
     'batch_size_test': 1,
     'balance_ratio': 0.975,
-    'im_height': 256,
-    'im_width': 256,
+    'im_height': 512,
+    'im_width': 512,
+    'output_height': 448,
+    'output_width': 448,
     'num_gpus': 1,
     'TOWER_NAME': 'tower',
     'standardizer': SamplewiseStandardizerTF(clip=6),
+    #'standardizer': SamplewiseStandardizer(clip=6),
     # 'balance_weights' via data_set
     'final_balance_weights': np.array([1 for i in range(0, num_classes)], dtype=float),
     'l2_reg': 0.0005,
@@ -31,7 +35,6 @@ cnf = {
         'do_flip': True,
         'allow_stretch': True,
     },
-    ),
     'num_epochs': 451,
     'lr_policy': PolyDecayPolicy(0.00005),
     # 'lr_policy': StepDecayPolicy({0: 0.0002, 100: 0.0002, 200: 0.0002, 400: 0.0002, 500: 0.0001}),
