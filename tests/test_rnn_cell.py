a70191eecb773ee9dc2418a8db8c549c3c339e2d
n3011 <mrinal.haloi11@gmail.com>
2017-10-17 00:37:01 +0530

    test for extended rnn cell

--- tests/test_rnn_cell.py
+++ tests/test_rnn_cell.py
@@ -315,6 +315,120 @@ class RNN_CellTest(tf.test.TestCase):
                 self.assertAllClose(res[1].c, expected_state_c)
                 self.assertAllClose(res[1].h, expected_state_h)
 
+    def test_without_residuals(self):
+        inputs = tf.constant(np.random.randn(1, 2), dtype=tf.float32)
+        state = (tf.constant(np.random.randn(1, 2), dtype=tf.float32),
+                 tf.constant(np.random.randn(1, 2), dtype=tf.float32))
+
+        with tf.variable_scope("root", initializer=tf.constant_initializer(0.5)):
+            standard_cell = rnn_cell.MultiRNNCell(
+                [rnn_cell.GRUCell(2, None, w_init=tf.constant_initializer(0.5)) for _ in range(2)], state_is_tuple=True)
+            res_standard = standard_cell(inputs, state, scope="standard")
+
+            test_cell = rnn_cell.ExtendedMultiRNNCell(
+                [rnn_cell.GRUCell(2, None, w_init=tf.constant_initializer(0.5)) for _ in range(2)])
+            res_test = test_cell(inputs, state, scope="test")
+
+        with self.test_session() as sess:
+            sess.run([tf.global_variables_initializer()])
+            res_standard_, res_test_, = sess.run([res_standard, res_test])
+
+        # Make sure it produces the same results as the standard cell
+        self.assertAllClose(res_standard_[0], res_test_[0])
+        self.assertAllClose(res_standard_[1][0], res_test_[1][0])
+        self.assertAllClose(res_standard_[1][1], res_test_[1][1])
+
+    def _test_with_residuals(self, inputs, **kwargs):
+        """Runs the cell in a session"""
+        inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
+        state = (tf.constant(np.random.randn(1, 2), dtype=tf.float32),
+                 tf.constant(np.random.randn(1, 2), dtype=tf.float32))
+
+        with tf.variable_scope("root", initializer=tf.constant_initializer(0.5)):
+            test_cell = rnn_cell.ExtendedMultiRNNCell(
+                [rnn_cell.GRUCell(
+                    2, None, w_init=tf.constant_initializer(0.5)) for _ in range(2)],
+                residual_connections=True,
+                **kwargs)
+            res_test = test_cell(inputs, state, scope="test")
+
+        with self.test_session() as sess:
+            sess.run([tf.global_variables_initializer()])
+            return sess.run(res_test)
+
+    def _test_constant_shape(self, combiner):
+        """Tests a residual combiner whose shape doesn't change
+        with depth"""
+        inputs = np.random.randn(1, 2)
+        with tf.variable_scope("same_input_size"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner=combiner)
+            self.assertEqual(res_[0].shape, (1, 2))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
+        inputs = np.random.randn(1, 5)
+        with tf.variable_scope("diff_input_size"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner=combiner)
+            self.assertEqual(res_[0].shape, (1, 2))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
+        with tf.variable_scope("same_input_size_dense"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner=combiner, residual_dense=True)
+            self.assertEqual(res_[0].shape, (1, 2))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
+        inputs = np.random.randn(1, 5)
+        with tf.variable_scope("diff_input_size_dense"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner=combiner, residual_dense=True)
+            self.assertEqual(res_[0].shape, (1, 2))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
+    def test_residuals_mean(self):
+        self._test_constant_shape(combiner="mean")
+
+    def test_residuals_add(self):
+        self._test_constant_shape(combiner="add")
+
+    def test_residuals_concat(self):
+        inputs = np.random.randn(1, 2)
+        with tf.variable_scope("same_input_size"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner="concat")
+            self.assertEqual(res_[0].shape, (1, 6))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
+        inputs = np.random.randn(1, 5)
+        with tf.variable_scope("diff_input_size"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner="concat")
+            self.assertEqual(res_[0].shape, (1, 5 + 2 + 2))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
+        inputs = np.random.randn(1, 2)
+        with tf.variable_scope("same_input_size_dense"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner="concat", residual_dense=True)
+            self.assertEqual(res_[0].shape, (1, 2 + 4 + 2))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
+        inputs = np.random.randn(1, 5)
+        with tf.variable_scope("diff_input_size_dense"):
+            res_ = self._test_with_residuals(
+                inputs, residual_combiner="concat", residual_dense=True)
+            self.assertEqual(res_[0].shape, (1, 2 + (5 + 2) + 5))
+            self.assertEqual(res_[1][0].shape, (1, 2))
+            self.assertEqual(res_[1][1].shape, (1, 2))
+
 
 if __name__ == '__main__':
     tf.test.main()
