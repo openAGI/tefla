18e795e11baa4f9649f5485afe732fa14d64ce8a
n3011 <mrinal.haloi11@gmail.com>
2017-10-19 01:06:03 +0530

    lookup data from vocab file

--- tefla/dataset/vocabulary.py
+++ tefla/dataset/vocabulary.py
@@ -2,7 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
 import tensorflow as tf
+from tensorflow import gfile
+
+SpecialVocab = collections.namedtuple("SpecialVocab",
+                                      ["UNK", "SEQUENCE_START", "SEQUENCE_END"])
 
 
 class Vocabulary(object):
@@ -14,6 +19,7 @@ class Vocabulary(object):
                  end_word="</S>",
                  unk_word="<UNK>"):
         """Initializes the vocabulary.
+
         Args:
             vocab_file: File containing the vocabulary, where the words are the first
               whitespace-separated token on each line (other tokens are ignored) and
@@ -47,6 +53,7 @@ class Vocabulary(object):
 
     def word_to_id(self, word):
         """Returns the integer word id of a word string.
+
         Args:
             word: `str`, input word
 
@@ -60,6 +67,7 @@ class Vocabulary(object):
 
     def id_to_word(self, word_id):
         """Returns the word string of an integer word id.
+
         Args:
             word: `int`, input word_id
 
@@ -70,3 +78,101 @@ class Vocabulary(object):
             return self.reverse_vocab[self.unk_id]
         else:
             return self.reverse_vocab[word_id]
+
+
+class VocabInfo(
+    collections.namedtuple("VocbabInfo",
+                           ["path", "vocab_size", "special_vocab"])):
+    """Convenience structure for vocabulary information.
+    """
+
+    @property
+    def total_size(self):
+        """Returns size the the base vocabulary plus the size of extra vocabulary"""
+        return self.vocab_size + len(self.special_vocab)
+
+
+def get_vocab_info(vocab_path):
+    """Creates a `VocabInfo` instance that contains the vocabulary size and
+      the special vocabulary for the given file.
+
+    Args:
+      vocab_path: Path to a vocabulary file with one word per line.
+
+    Returns:
+      A VocabInfo tuple.
+    """
+    with gfile.GFile(vocab_path) as file:
+        vocab_size = sum(1 for _ in file)
+    special_vocab = get_special_vocab(vocab_size)
+    return VocabInfo(vocab_path, vocab_size, special_vocab)
+
+
+def get_special_vocab(vocabulary_size):
+    """Returns the `SpecialVocab` instance for a given vocabulary size.
+    """
+    return SpecialVocab(*range(vocabulary_size, vocabulary_size + 3))
+
+
+def create_vocabulary_lookup_table(filename, default_value=None):
+    """Creates a lookup table for a vocabulary file.
+
+    Args:
+      filename: Path to a vocabulary file containg one word per line.
+        Each word is mapped to its line number.
+      default_value: UNK tokens will be mapped to this id.
+        If None, UNK tokens will be mapped to [vocab_size]
+
+      Returns:
+        A tuple (vocab_to_id_table, id_to_vocab_table,
+        word_to_count_table, vocab_size). The vocab size does not include
+        the UNK token.
+      """
+    if not gfile.Exists(filename):
+        raise ValueError("File does not exist: {}".format(filename))
+
+    # Load vocabulary into memory
+    with gfile.GFile(filename) as file:
+        vocab = list(line.strip("\n") for line in file)
+    vocab_size = len(vocab)
+
+    has_counts = len(vocab[0].split("\t")) == 2
+    if has_counts:
+        vocab, counts = zip(*[_.split("\t") for _ in vocab])
+        counts = [float(_) for _ in counts]
+        vocab = list(vocab)
+    else:
+        counts = [-1. for _ in vocab]
+
+    # Add special vocabulary items
+    special_vocab = get_special_vocab(vocab_size)
+    vocab += list(special_vocab._fields)
+    vocab_size += len(special_vocab)
+    counts += [-1. for _ in list(special_vocab._fields)]
+
+    if default_value is None:
+        default_value = special_vocab.UNK
+
+    tf.logging.info("Creating vocabulary lookup table of size %d", vocab_size)
+
+    vocab_tensor = tf.constant(vocab)
+    count_tensor = tf.constant(counts, dtype=tf.float32)
+    vocab_idx_tensor = tf.range(vocab_size, dtype=tf.int64)
+
+    # Create ID -> word mapping
+    id_to_vocab_init = tf.contrib.lookup.KeyValueTensorInitializer(
+        vocab_idx_tensor, vocab_tensor, tf.int64, tf.string)
+    id_to_vocab_table = tf.contrib.lookup.HashTable(id_to_vocab_init, "UNK")
+
+    # Create word -> id mapping
+    vocab_to_id_init = tf.contrib.lookup.KeyValueTensorInitializer(
+        vocab_tensor, vocab_idx_tensor, tf.string, tf.int64)
+    vocab_to_id_table = tf.contrib.lookup.HashTable(vocab_to_id_init,
+                                                    default_value)
+
+    # Create word -> count mapping
+    word_to_count_init = tf.contrib.lookup.KeyValueTensorInitializer(
+        vocab_tensor, count_tensor, tf.string, tf.float32)
+    word_to_count_table = tf.contrib.lookup.HashTable(word_to_count_init, -1)
+
+    return vocab_to_id_table, id_to_vocab_table, word_to_count_table, vocab_size
