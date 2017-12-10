2aa475e978eef14e63227683daa2fe79853bc9dd
n3011 <mrinal.haloi11@gmail.com>
2017-12-10 15:59:10 +0530

    multitask opt wrapper

--- tefla/core/optimizer.py
+++ tefla/core/optimizer.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import types
+import six
 import contextlib
 import numpy as np
 import numbers
@@ -967,3 +969,112 @@ class VariableClippingOptimizer(optimizer.Optimizer):
                 yield
         else:
             yield
+
+
+def _is_all_zeros(grad):
+    all_zeros = tf.equal(tf.count_nonzero(grad), 0)
+    return all_zeros
+
+
+def _get_wrapper(fn, opt):
+
+    def wrapper(self, grad, *args, **kwargs):
+        all_zeros = _is_all_zeros(grad)
+        return tf.cond(all_zeros, tf.no_op,
+                       lambda: fn(grad, *args, **kwargs))
+
+    wrapper = types.MethodType(wrapper, opt)
+    return wrapper
+
+
+class MultitaskOptimizer(object):
+    """Optimizer wrapper making all-zero gradients harmless.
+
+    This might be useful when a multi-task loss is used,
+    and some components of the loss might be
+    not present (e.g. masked out) in some training batches.
+    Technically their gradient would be zero,
+    which would normally affect the optimizer state
+    (e.g. push running average to zero).
+    However this is not the desired behaviour,
+    since the missing loss component
+    should be treated as unknown rather than zero.
+
+    This wrapper filters out all-zero gradient tensors,
+    therefore preserving the optimizer state.
+
+    If gradient clipping by global norm is used,
+    the provided function clip_gradients_by_global_norm
+    should be used (and specified explicitly by the user).
+    Otherwise the global norm would be underestimated
+    because of all-zero tensors that should be ignored.
+
+    The gradient calculation and application
+    are delegated to an underlying optimizer.
+    The gradient application is altered only for all-zero tensors.
+
+    Example:
+    ```python
+    momentum_optimizer = tf.train.MomentumOptimizer(
+      learning_rate, momentum=0.9)
+    multitask_momentum_optimizer = tefla.core.optimizer.MultitaskOptimizer(
+      momentum_optimizer)
+    gradvars = multitask_momentum_optimizer.compute_gradients(
+      loss)
+    gradvars_clipped, _ = tefla.core.optimizer.clip_gradients_by_global_norm(
+      gradvars, 15.0)
+    train_op = multitask_momentum_optimizer.apply_gradients(
+      gradvars_clipped, global_step=batch)
+    ```
+    """
+
+    def __init__(self, opt):
+        """Constructor.
+
+        Args:
+          opt: an instance of a class that implements tf.train.Optimizer.
+        """
+        if not isinstance(opt, optimizer.Optimizer):
+            raise TypeError(
+                'Supplied optimizer must be an instance of tf.train.Optimizer')
+        self._opt = opt
+        overridden_methods = ('_apply_dense', '_resource_apply_dense',
+                              '_apply_sparse', '_resource_apply_sparse')
+        for name in overridden_methods:
+            fn = getattr(self._opt, name)
+            wrapper = _get_wrapper(fn, self._opt)
+            setattr(self._opt, name, wrapper)
+
+    def __getattr__(self, name):
+        return getattr(self._opt, name)
+
+
+def clip_gradients_by_global_norm(gradients_variables, clip_norm=20.):
+    """Clips gradients of a multitask loss by their global norm.
+
+    Ignores all-zero tensors when computing the global norm.
+
+    Args:
+      gradients_variables: a list of pairs (gradient, variable).
+      clip_norm: a float Tensor, the global norm to clip on. Default is 20.0.
+
+    Returns:
+      list: A list of pairs of the same type as gradients_variables,.
+      fixed_global_norm: A 0-D (scalar) Tensor representing the global norm.
+    """
+    gradients, variables = six.moves.zip(*gradients_variables)
+
+    def _replace_nonexisting_grad(grad):
+        if grad is None:
+            return grad
+        all_zeros = _is_all_zeros(grad)
+        return tf.cond(
+            all_zeros,
+            lambda: tf.zeros([], dtype=tf.as_dtype(grad.dtype)),
+            lambda: grad)
+
+    nonzero_gradients = [_replace_nonexisting_grad(g) for g in gradients]
+    fixed_global_norm = tf.global_norm(nonzero_gradients)
+    gradients, _ = tf.clip_by_global_norm(
+        gradients, clip_norm, use_norm=fixed_global_norm)
+    return list(six.moves.zip(gradients, variables)), fixed_global_norm
