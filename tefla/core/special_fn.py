2bcd3f1096c8c7fe3060eea0bcde2f8d6f5df35a
n3011 <mrinal.haloi11@gmail.com>
2018-01-10 00:49:49 +0530

    add fn for shape

--- tefla/core/special_fn.py
+++ tefla/core/special_fn.py
@@ -1308,6 +1308,26 @@ def clip_variables(optimizer, variables, weight_clip):
         colocate_clip_ops_with_vars=True)
 
 
+def shape_list(x):
+    """Return list of dims, statically where possible."""
+    x = tf.convert_to_tensor(x)
+
+    # If unknown rank, return dynamic shape
+    if x.get_shape().dims is None:
+        return tf.shape(x)
+
+    static = x.get_shape().as_list()
+    shape = tf.shape(x)
+
+    ret = []
+    for i in xrange(len(static)):
+        dim = static[i]
+        if dim is None:
+            dim = shape[i]
+        ret.append(dim)
+    return ret
+
+
 def approximate_split(x, num_splits, axis=0):
     """Split approximately equally into num_splits parts.
 
