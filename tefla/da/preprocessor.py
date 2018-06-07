7c1ad77156e8d8172141bc6ccbc0d184c6196185
n3011 <mrinal.haloi11@gmail.com>
2018-06-08 00:32:28 +0530

    fix python2 error

--- tefla/da/preprocessor.py
+++ tefla/da/preprocessor.py
@@ -1794,34 +1794,36 @@ class SegPreprocessorv2(Preprocessor):
 
 class RexPreprocessor(Preprocessor):
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+  def __init__(self, width, height, standardizer):
+    self.width = width
+    self.height = height
+    self.standardizer = standardizer
 
-    def process(self, frame):
-        roi_height, roi_width = frame.shape[0], int(frame.shape[1] * .68)
-        processed = np.zeros((roi_height, roi_width))
+  def process(self, frame):
+    roi_height, roi_width = frame.shape[0], int(frame.shape[1] * 0.68)
+    processed = np.zeros((roi_height, roi_width))
 
-        roi = frame[:, :roi_width, 0]
-        all_obstacles_idx = roi > 50
-        processed[all_obstacles_idx] = 1
-        unharmful_obstacles_idx = roi > 200
-        processed[unharmful_obstacles_idx] = 0
+    roi = frame[:, :roi_width, 0]
+    all_obstacles_idx = roi > 50
+    processed[all_obstacles_idx] = 1
+    unharmful_obstacles_idx = roi > 200
+    processed[unharmful_obstacles_idx] = 0
 
-        processed = imresize(processed, (self.height, self.width, 1))
-        processed = processed / 255.0
-        return processed
+    processed = np.asarray(imresize(processed, (self.height, self.width, 1)), dtype=np.float)
+    return processed
 
-    def preprocess_for_train(self, inputs):
-        return self.process(inputs)
+  def preprocess_for_train(self, inputs):
+    processed = self.process(inputs)
+    return self.standardizer(processed, True)
 
-    def preprocess_for_eval(self, inputs):
-        return self.process(inputs)
+  def preprocess_for_eval(self, inputs):
+    processed = self.process(inputs)
+    return self.standardizer(processed, False)
 
-    def get_initial_state(self, first_frame):
-        self.state = np.array([first_frame, first_frame, first_frame, first_frame])
-        return self.state
+  def get_initial_state(self, first_frame):
+    self.state = np.array([first_frame, first_frame, first_frame, first_frame])
+    return self.state
 
-    def get_updated_state(self, next_frame):
-        self.state = np.array([*self.state[-3:], next_frame])
-        return self.state
+  def get_updated_state(self, next_frame):
+    self.state = np.concatenate((self.state[-3:], np.expand_dims(next_frame, axis=0)), axis=0)
+    return self.state
