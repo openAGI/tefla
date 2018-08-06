83eb6496376d7f4992b11936a9a01259ca59c314
n3011 <mrinal.haloi11@gmail.com>
2018-08-06 14:03:37 +0530

    add script to install modules

---
+++ tools/ci_build/install/install_pip_packages.sh
@@ -0,0 +1,31 @@
+# Copyright 2018 The Tefla Authors. All Rights Reserved.
+pip install yapf==0.22.0
+pip install coveralls
+pip install requests[security]
+pip install pytest==3.6.1
+pip install pep8==1.7.0
+pip install -U pytest-cov
+pip install --upgrade pip setuptools wheel
+pip install entrypoints
+pip install --only-binary=numpy,scipy numpy scipy
+pip install Cython==0.27.1
+pip install pyaml==17.12.1
+pip install pydensecrf==1.0rc3 
+pip install tensorflow==1.8.0
+pip install opencv-python==3.4.1.15 
+pip install mock==2.0.0 
+pip install pylint==1.9.2
+pip install Pillow==2.3.0
+pip install pandas==0.18.1
+pip install SharedArray==3.0.0
+pip install click==6.6
+pip install scikit-image==0.12.3
+pip install scikit-learn==0.18.2
+pip install scipy==0.18.0
+pip install six==1.10.0
+pip install setuptools==28.8.0
+pip install jupyter==1.0.0
+pip install ghalton==0.6
+pip install progress
+pip install h5py
+pip install git+https://github.com/openAGI/tefla.git
---
+++ tools/ci_build/install/install_python3_6.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Copyright 2018 The Tefla Authors. All Rights Reserved.
+
+apt-get update && apt-get install -y --no-install-recommends apt-utils
+apt-get install pkg-config
+apt-get update && apt-get install -y --no-install-recommends apt-utils
+apt-get install -y wget \
+  && rm -rf /var/lib/apt/lists/*
+apt-get update
+apt-get install -y software-properties-common vim
+add-apt-repository ppa:jonathonf/python-3.6
+apt-get update
+
+apt-get install -y build-essential python3.6 python3.6-dev python3-pip python3.6-venv python3-tk
+apt-get install -y git
+apt install -y libsm6 libxext6
+apt-get install -y libxrender-dev
+
+# update pip
+python3.6 -m pip install pip --upgrade
+python3.6 -m pip install wheel
