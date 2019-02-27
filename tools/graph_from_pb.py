a65b3a9f1031f2301672fc719c45f622ddd952b7
mrinalhaloi <mrinal.haloi@nagarro.com>
2019-02-27 22:28:09 +0530

    improving graph conversion tool

--- tools/graph_from_pb.py
+++ tools/graph_from_pb.py
@@ -1,14 +1,22 @@
 import click
-from tefla.utils.graph_utils import graph_from_pb
+import tensorflow as tf
+
+from tefla.utils.graph_utils import graph_from_pb, graph_from_pb_v2
 
 
 @click.command()
 @click.option('--graph_filename', default='', show_default=True, help='Input graph (.pb) filename')
 @click.option(
-    '--logdir', default='log', show_default=True, help='Directoey to store the summary log')
+    '--logdir', default='log', show_default=True, help='Directory to store the summary log')
 def main(graph_filename, logdir):
-  graph_from_pb(graph_filename, logdir)
+  tf.logging.info('Extracting graph from pb file.')
+  try:
+    graph_from_pb(graph_filename, logdir)
+  except Exception:
+    graph_from_pb_v2(graph_filename, logdir)
+  tf.logging.info('Extracting graph from pb file is successful.')
 
 
 if __name__ == '__main__':
+  tf.logging.set_verbosity(tf.logging.INFO)
   main()
