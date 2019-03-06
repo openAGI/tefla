f0b12f24e6e5bb53f6bee2907c6bf47305706ed1
rajalakshmi139 <rajarajalakshmi.k@gmail.com>
2019-03-06 13:06:16 +0530

    added data_utils file after error correction

--- tefla/utils/data_utils.py
+++ tefla/utils/data_utils.py
@@ -7,9 +7,12 @@ import csv
 
 
 class Data_utils:
-  """ Class creates dataset required for training"""
+  """ 
+    Class creates dataset required for training
+  """
 
   def __init__(self, input_csv_path, input_filepath, output_path, train_percentage, val_percentage):
+
     self.traindata = 'train_dataset'
     self.valdata = 'val_dataset'
     self.testdata = 'test_dataset'
@@ -21,10 +24,7 @@ class Data_utils:
 
   def createDirectoryForDataset(self, dataname):
     """
-    createDirectoryForDataset function creates directory if it is not created by the user,
-    for storing the dataset split for training,validation and test
-    Args:
-    path: path of the file that contains all the imagenames and groundtruth
+    Function to create directiry for dataset.
     """
     try:
       os.makedirs(os.path.join(self.output_path, dataname))
@@ -32,7 +32,9 @@ class Data_utils:
       pass
 
   def create_datasets(self):
-    """ create_datasets function call the functiosn that are used for creating the datasets """
+    """
+    Function to create datasets 
+    """
     self.createdtraindata, self.createdval_data, self.createdtest_data = self.split_dataset()
     traindata = self.create_traindataset()
     valdata = self.create_validationdataset()
@@ -40,13 +42,10 @@ class Data_utils:
 
   def split_dataset(self):
     """
-    split_dataset function splits the data into training set, validation set and test set
-    Args:
-    imagelabelpath: path of the label, accepts csv file
-    training_percent: percentage value to split the data for the training set
-    val_percentage: percentage value to split the data for the validation set
+    Function to split the data into training set, validation set and test set
     Returns:
-    train_filenames,val_filenames and test_filenames as list
+     train_filenames,val_filenames and test_filenames as list
+
     """
 
     file = open(self.input_csv_path, "r")
@@ -67,12 +66,10 @@ class Data_utils:
     return train_filenames, val_filenames, test_filenames
 
   def create_traindataset(self):
-    """ 
-    create_traindataset function stores the data split by the createdata function in the location specified by the user
-    Args:
-    createdtraindata: traindata returned as list by createdata function
-    original_path: path of the source directory from where the images are copied to train dataset
-    train_datapath: path where the train dataset is to stored 
+    """
+    Function stores the split train data in specified location
+    Returns:
+      None 
     """
     self.createDirectoryForDataset(self.traindata)
     for i in self.createdtraindata:
@@ -84,11 +81,9 @@ class Data_utils:
 
   def create_validationdataset(self):
     """ 
-    create_validationdataset function stores the split data in the location specified by the user
-    Args:
-    createdval_data: traindata returned as list by createdata function
-    source_path:path of the source directory from where the images are copied to validation dataset
-    valdatapath: path where the validation dataset is to stored
+    Function to store the split validation data in the specified location
+    Returns:
+      None
     """
     self.createDirectoryForDataset(self.valdata)
     for i in self.createdval_data:
@@ -100,11 +95,9 @@ class Data_utils:
 
   def create_testdataset(self):
     """
-    create_testdataset function stores the split data in the location specified by the user
-    Args:
-    createdtest_data: traindata returned as list by createdata function
-    sourcepath: path of the source directory from where the images are copied to test dataset
-    testdatapath: path where the test dataset is to stored
+    Function stores the split test data in the specified location
+    Returns:
+      None
     """
     self.createDirectoryForDataset(self.testdata)
     for i in self.createdtest_data:
@@ -113,11 +106,3 @@ class Data_utils:
       files = os.listdir(self.input_filepath)
       if testfilename in files:
         shutil.copy(self.input_filepath + testfilename, self.output_path + self.testdata)
-
-
-# datacall = Data_utils("/home/raji/Desktop/test_2.csv",
-#                       "/home/raji/Downloads/malaria/cell_images/test_p/",
-#                       "/home/raji/Desktop/dataset/", 0.8, 0.9)
-
-# print("the output is")
-# datacall.create_datasets()
